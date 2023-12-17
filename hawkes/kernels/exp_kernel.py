d614c53cee9a7eef273a298894081d7d48e9e161
小島宗一郎 <s_kojima@Moonflower.local>
2023-12-18 07:55:53 +0900

    Add: 推定👉1次元指数カーネル

--- hawkes/kernels/exp_kernel.py
+++ hawkes/kernels/exp_kernel.py
@@ -2,9 +2,9 @@ import numpy as np
 from .base import BaseKernel
 
 class ExpKernel(BaseKernel):
-    def __init__(self, alpha: float, beta: float):
-        self._alpha = alpha
-        self._beta = beta
+    def __init__(self, a: float, b: float):
+        self._a = a
+        self._b = b
 
     def __call__(self, t_s):
-        return self._alpha * self._beta * np.exp(-self._beta * t_s)
+        return self._a * self._b * np.exp(-self._b * t_s)
